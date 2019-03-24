bcfd8f5f90bb2be8c370e707693d13ed4eb18dc2
Victoria B <viktorija.bourassa@gmail.com>
2019-03-24 14:14:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  patch 'posts/:id', to: 'posts#update'
-  resources :posts only:[:new, :update, :create, :show]# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts only: [:new, :update, :create, :show]# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
